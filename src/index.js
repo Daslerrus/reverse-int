78e5807e029968143177db716ffdb935e33e94f3
Kuzin Artyom <pspman1998@gmail.com>
2023-01-12 19:27:17 +0600

    init: started task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let strNum = String(n).split('').reverse().join('');
+    if (strNum[strNum.length - 1] === '-') {
+        strNum = strNum.substring(0, strNum.length - 1);
+    }
+    return Number(strNum);
 }
+
